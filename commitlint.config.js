561bfe9df5c0ee6603a929e637853bdb72a3266e
lijianqiang <>
2020-08-18 22:48:33 +0800

    feat: 代码规范

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,5 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'type-enum': [2, 'always', ["feat", "fix", "readme", "init"]],
-        'subject-full-stop': [0, 'never'],
-        'subject-empty': [0, 'never']
     }
 };
