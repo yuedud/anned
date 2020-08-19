3d2aa74c629c94d6a96c27e3e28b4fa406a3ad08
lijianqiang01 <lijianqiang01@baidu.com>
2020-08-19 11:01:58 +0800

    fix: 修改webpack错误

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
 module.exports = {
     entry: './src/root.js',
     output: {
