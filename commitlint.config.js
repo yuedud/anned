c34189af4b6c740758a008988a74d564cf8384d4
lijianqiang01 <lijianqiang01@baidu.com>
2020-08-19 10:06:58 +0800

    fix: 增加commit检测

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', ["feat", "fix", "readme", "init"]],
+        'type-enum': [2, 'always', [
+            "feat", "fix", 'readme', 'init'
+        ]],
         'subject-full-stop': [0, 'never'],
-        'subject-empty': [0, 'never']
+        'subject-case': [0, 'never'],
     }
 };
